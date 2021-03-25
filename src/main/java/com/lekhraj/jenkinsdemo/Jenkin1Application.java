e052fd5822d5400667d94a504d8847c62ec0921e
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-03-25 18:14:57 +0530

    first commit

---
+++ src/main/java/com/lekhraj/jenkinsdemo/Jenkin1Application.java
@@ -0,0 +1,13 @@
+package com.lekhraj.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkin1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jenkin1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lekhraj/jenkinsdemo/Jenkin1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lekhraj.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkin1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
